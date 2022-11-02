a67d44b4b80d0869548df8e2b6bd27ab2b47d734
itmm-Andrianov-Gleb-FIIT3 <glebius.gleb@yandex.ru>
2022-11-02 20:50:46 +0300

    utf-8

--- HW1-2/TRI/THREE.c
+++ HW1-2/TRI/THREE.c
@@ -9,14 +9,10 @@ int main() {
 	printf("%s\n", "");
 	printf("%s\n", "");
 	printf("%s\n", ".................................");
-	printf("%s", ".");
-	printf("%32s\n", ".");
-	printf("%s", ".");
-	printf("%32s\n", ".");
-	printf("%s", ".");
-	printf("%32s\n", ".");
-	printf("%s", ".");
-	printf("%32s\n", ".");
+	for (int g; g < 4; g++){
+		printf("%s", ".");
+		printf("%32s\n", ".");
+	}
 	printf("%s\n", ".................................");
 	printf("%s\n", "");
 	printf("%s\n", "");
@@ -28,31 +24,13 @@ int main() {
 	printf("%s\n", "");
 	printf("%s\n", "");
 	printf("%s\n", ".................................");
-	printf("%s", ".");
-	printf("%8s", "|");
-	printf("%8s", "|");
-	printf("%8s", "|");
-	printf("%8s\n", ".");
-	printf("%s", ".");
-	printf("%8s", "|");
-	printf("%8s", "|");
-	printf("%8s", "|");
-	printf("%8s\n", ".");
-	printf("%s", ".");
-	printf("%8s", "|");
-	printf("%8s", "|");
-	printf("%8s", "|");
-	printf("%8s\n", ".");
-	printf("%s", ".");
-	printf("%8s", "|");
-	printf("%8s", "|");
-	printf("%8s", "|");
-	printf("%8s\n", ".");
-	printf("%s", ".");
-	printf("%8s", "|");
-	printf("%8s", "|");
-	printf("%8s", "|");
-	printf("%8s\n", ".");
+	for (int i; i < 5;i++) {
+		printf("%s", ".");
+		printf("%8s", "|");
+		printf("%8s", "|");
+		printf("%8s", "|");
+		printf("%8s\n", ".");
+	}
 	printf("%s\n", ".................................");
 	system("pause");
 	return 0;
