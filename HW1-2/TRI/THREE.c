567fd1336f0b64151238029fd64a15c11d6c4804
itmm-Andrianov-Gleb-FIIT3 <glebius.gleb@yandex.ru>
2022-11-16 08:39:43 +0300

    utf-8

--- HW1-2/TRI/THREE.c
+++ HW1-2/TRI/THREE.c
@@ -1,37 +1,71 @@
 #include<stdio.h>
 #include<limits.h>
 int main() {
-	printf("%s\n", ".");
-	printf("%s\n", ".........");
-	printf("%s\n", ".................");
-	printf("%s\n", ".........................");
+	printf(".\n");
+	printf(".........\n");
+	printf(".................\n");
+	printf(".........................\n");
+	printf(".................................\n");
+
+	printf("\n");
+	printf("\n");
+
+	printf(".................................\n");
+	printf(".");
+	printf("%33s", ".\n");
+	printf(".");
+	printf("%33s", ".\n");
+	printf(".");
+	printf("%33s", ".\n");
+	printf(".");
+	printf("%33s", ".\n");
+	printf(".................................\n");
+
+	printf("\n");
+	printf("\n");
+
+	printf("%34s", ".\n");
+	printf("%34s", ".........\n");
+	printf("%34s", ".................\n");
+	printf("%34s", ".........................\n");
+	printf("%34s", ".................................\n");
+
+	printf("\n");
+	printf("\n");
+
 	printf("%s\n", ".................................");
-	printf("%s\n", "");
-	printf("%s\n", "");
+	printf(".");
+	printf("%8s", "|");
+	printf("%8s", "|");
+	printf("%8s", "|");
+	printf("%8s\n", ".");
+
+	printf(".");
+	printf("%8s", "|");
+	printf("%8s", "|");
+	printf("%8s", "|");
+	printf("%8s\n", ".");
+
+	printf(".");
+	printf("%8s", "|");
+	printf("%8s", "|");
+	printf("%8s", "|");
+	printf("%8s\n", ".");
+
+	printf(".");
+	printf("%8s", "|");
+	printf("%8s", "|");
+	printf("%8s", "|");
+	printf("%8s\n", ".");
+
+	printf(".");
+	printf("%8s", "|");
+	printf("%8s", "|");
+	printf("%8s", "|");
+	printf("%8s\n", ".");
+
 	printf("%s\n", ".................................");
-	for (int g; g < 4; g++){
-		printf("%s", ".");
-		printf("%32s\n", ".");
-	}
-	printf("%s\n", ".................................");
-	printf("%s\n", "");
-	printf("%s\n", "");
-	printf("%33s\n", ".");
-	printf("%33s\n", ".........");
-	printf("%33s\n", ".................");
-	printf("%33s\n", ".........................");
-	printf("%33s\n", ".................................");
-	printf("%s\n", "");
-	printf("%s\n", "");
-	printf("%s\n", ".................................");
-	for (int i; i < 5;i++) {
-		printf("%s", ".");
-		printf("%8s", "|");
-		printf("%8s", "|");
-		printf("%8s", "|");
-		printf("%8s\n", ".");
-	}
-	printf("%s\n", ".................................");
+
 	system("pause");
 	return 0;
 }
